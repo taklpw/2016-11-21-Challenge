f9859966478e340a1b2208ffb1cf0b59e1f6759c
Kelly Lynch <kel555222@gmail.com>
2016-11-25 17:00:26 +1100

    Renamed file and conformed to PEP8... A bit.

--- defuser.py
+++ defuser.py
@@ -4,55 +4,57 @@
 # Challenge: https://www.reddit.com/r/dailyprogrammer/comments/5e4mde/20161121_challenge_293_easy_defusing_the_bomb/
 
 # Function to create an array of user inputs of colours in order in which they are cut.
-def getInput():
+
+
+def getinput():
     # Variable Declarations
-    validInputs = ['white', 'red', 'black', 'orange', 'green', 'purple', 'q']
-    inputValid = 0
-    colourList = []
-    arrayIndex = 0
-    loops = 0
-    keepLooping = 1
+    validinputs = ['white', 'red', 'black', 'orange', 'green', 'purple', 'q']
+    goodvalue = 0
+    colourlist = []
+    arrayindex = 0
+    keeplooping = 1
 
     # Instructions to user
     print "Enter colours in the order of cut"
     print "Enter Colour or 'q' to stop"
 
     # Infinite Loop
-    while keepLooping:
+    while keeplooping:
         # Get Input and test to see if it's valid, if it is add it to the array, if not notify the user
-        colourList.append(raw_input())
-        for validInput in validInputs:
-            if colourList[arrayIndex] == validInput:
-                inputValid = 1
+        colourlist.append(raw_input())
+        for validInput in validinputs:
+            if colourlist[arrayindex] == validInput:
+                goodvalue = 1
                 break
             else:
-                inputValid = 0
+                goodvalue = 0
 
-        if inputValid == 1:
+        if goodvalue == 1:
             # If 'q' is entered remove 'q' from array and exit user input
-            if colourList[arrayIndex] == 'q':
-                keepLooping = 0
-                colourList.pop()
+            if colourlist[arrayindex] == 'q':
+                keeplooping = 0
+                colourlist.pop()
             # Get ready for the next input
-            arrayIndex += 1
+            arrayindex += 1
         else:
             # Alert user and remove invalid entry from the list
             print "Not a valid input"
-            colourList.pop()
+            colourlist.pop()
 
     # Display the entered list of colours to the user for verification
     print "Your colours are the following (in order):"
-    for colour in colourList:
+    for colour in colourlist:
         if colour != 'q':
             print colour
 
-    return colourList
+    return colourlist
 
 
 # Function to determine if the bomb is sucessfully disarmed or exploded.
 # TODO: Determine if colour list is valid or not
-def disarm(passedColours):
+def disarm(passedcolours):
     print "boom"
     print "disarmed"
 
-disarm(getInput())
+
+disarm(getinput())
